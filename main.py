631d18549e60a83d30a459d3eb9a9106fd873134
Sebastian Estrada <est21405@uvg.edu.gt>
2024-04-09 00:07:11 -0600

    inciso 1,2,3 finalizado

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from neo4j import GraphDatabase
+from py2neo import Graph, Node, Relationship
 import fake as fk
 import random as rm
 
@@ -7,39 +8,22 @@ username = "neo4j"
 password = "RAAmJuO5zwL3JdWn52oMcLcHrQya8xswbcvxBhIBcsY"
 
 driver = GraphDatabase.driver(uri, auth=(username, password))
+graph = Graph(uri, auth=(username, password))
 
 fake = fk.Faker()
 
-lstmvName = ["The Matrix", "The Matrix Reloaded", "The Matrix Revolutions", "The Matrix Resurrections"]
-lstmvId = ["tt0133093", "tt0234215", "tt0242653", "tt10838180"]
-lstmvYear = [1999, 2003, 2003, 2021]
+lstmvName = ["The Matrix", "The Matrix Reloaded", "The Matrix Revolutions", "The Matrix Resurrections", "Piratas del caribe"]
+lstmvId = ["tt0133093", "tt0234215", "tt0242653", "tt10838180", "tt0325980"]
+lstmvYear = [1999, 2003, 2003, 2021, 2003]
 lstmvPlot = ["A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
             "Neo and his allies", "The human city of Zion defends itself against the massive invasion of the machines as Neo fights to end the war at another front while also opposing the rogue Agent Smith.",
-            "Return to a world of two realities: one, everyday life; the other, what lies behind it. To find out if his reality is a physical or mental construct, to truly know himself, Mr. Anderson will have to choose to follow the white rabbit once more."]
+            "Return to a world of two realities: one, everyday life; the other, what lies behind it. To find out if his reality is a physical or mental construct, to truly know himself, Mr. Anderson will have to choose to follow the white rabbit once more.",
+            "Blacksmith Will Turner teams up with eccentric pirate 'Captain' Jack Sparrow to save his love, the governor's daughter, from Jack's former pirate allies, who are now undead."]
 lstusid = ["user1", "user2", "user3", "user4", "user5"]
 
 def runQuery(tx, query):
     result = tx.run(query)
     return result
-
-"""
-necesito crear un grafo en el que 
-esten los siguientes nodos:
-
-- User:
-    name: string
-    userid: string
-
-- Movie:
-    title: string
-    movieid: string
-    year: int
-    plot: string
-
-en los que user rated movie con las siguientes propiedades
-rating: int de 0-5
-timestamp: int
-"""
 
 def createMovie(tx, title, movieid, year, plot):
     tx.run("CREATE (movie:Movie {title: $title, movieid: $movieid, year: $year, plot: $plot})", title=title, movieid=movieid, year=year, plot=plot)
@@ -57,14 +41,36 @@ def createGraph(tx):
         createUser(tx, fake.name(), lstmvId[i])
         rateMovie(tx, lstmvId[i], lstmvId[i], rm.randint(0, 5), rm.randint(100, 300))
 
-with driver.session() as session:
-    session.write_transaction(createGraph)
-    result = session.read_transaction(runQuery, "MATCH (n) RETURN n")
-    for record in result:
-        print(record)
+
+def pushData():
+    with driver.session() as session:
+        session.write_transaction(createGraph)
+        
+    print("Graph created")
+
+    driver.close()
 
 
+def getUser(user):
+    query = "MATCH (n:User {name: '" + user + "'}) RETURN n"
 
+    result = graph.run(query)
+    return result
 
-driver.close()
+def getMovie(movie):
+    query = "MATCH (n:Movie {title: '" + movie + "'}) RETURN n"
 
+    result = graph
+    return result
+
+def getMovieRatedByUser(user):
+    query = "MATCH (u:User {name: '" + user + "'})-[r:RATED]->(m:Movie) RETURN m"
+
+    result = graph.run(query)
+    return result
+
+#pushData()
+
+#print(getUser("Guido Warsaw"))
+#print(getMovie("The Matrix")
+#print(getMovieRatedByUser("Guido Warsaw"))
